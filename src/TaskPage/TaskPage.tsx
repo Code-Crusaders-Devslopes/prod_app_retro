abbbff5c8cc2689113fd603829758f1bf6e7bbcf
Jesse Miller <jpmllr89@gmail.com>
2023-11-16 22:19:18 -0500

    added Sound to the delete, complete and add buttons

--- src/TaskPage/TaskPage.tsx
+++ src/TaskPage/TaskPage.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import style from './taskPage.module.css'
+import stomp from '../../public/smb_stomp.wav'
+import oneUp from '../../public/smb_1-up.wav'
+// import gameOver from '../../public/smb_gameover.wav'
+import coinSound from '../../public/smb_coin.wav'
 import {useState} from 'react';
 
 const TaskPage = () => {
@@ -19,19 +24,48 @@ const TaskPage = () => {
 
   const handleClick = () =>{
     // put functionality here
+    const audio = new Audio(coinSound);
+    audio.play();
+    // add to task list
+  }
+
+  const completeTask = () => {
+    // will play sound and also mark task as complete
+    const audio = new Audio(oneUp);
+    audio.play();
+  }
+
+  const clearAll = () => {
+    // will play sound and also clear all tasks
+    const audio = new Audio(gameOver);
+    audio.play();
+  }
+
+  const deleteTask = () => {
+    // will play sound and also delete the task
+    const audio = new Audio(stomp);
+    audio.play();
+  }
+
+  const mouseOver = (e) => {
+    e.target.innerText = `WHERE DO YOU THINK YOU'RE GOING?`
+  }
+
+  const mouseOut = (e) => {
+    e.target.innerText = `Home`
   }
 
   return (
     <>
-          <div className="nes-container">
+        <div className={`${style.backgroundColorWhite} + nes-container`}>
           <div>To do List</div>
-          <div><a href="/">Home</a></div>
+          <div><a onMouseOver={mouseOver} onMouseOut={mouseOut} href="/">Home</a></div>
           <div className={style.window}>
             <div className={style.main}>
               <div className={style.inputField}>
-                <input onChange={(e) => setInput(e.target.value)} type="text" id="input-box" className='nes.nes-input'></input>
-                <button onClick={() => handleClick()} type="button" id="add" className='nes-btn is-primary'>
-                  <p>Add to List</p> <i className='nes-icon coin'></i>
+                <input onChange={(e) => setInput(e.target.value)} type="text" id="input-box" className={`${style.maxWidthOut} ${style.alignItems} nes.nes-input`}></input>
+                <button onClick={() => handleClick()} type="button" id="add" className={`${style.flexRow} ${style.justifyContent} nes-btn is-primary`}>
+                  <p>Add</p> <i className='nes-icon coin'></i>
                 </button>
               </div>
               <img src="Koopa_Shell_Spin.webp" alt="" className={style.icon} />
@@ -39,8 +73,8 @@ const TaskPage = () => {
                 {tasks.map((x:any) =>
                   <div className={style.icon + 'nes-container'}>
                     <p>{x.task}</p>
-                    <span><img src="Koopa_Shell_Spin.webp" alt="delete" className={style.icon}/></span>
-                    <span><img src="mushroom.png" alt="complete" className={style.icon}/></span>
+                    <span><img src="Koopa_Shell_Spin.webp" alt="delete" className={style.icon} onClick={()=> deleteTask()}/></span>
+                    <span><img src="mushroom.png" alt="complete" className={style.icon} onClick={completeTask}/></span>
                   </div>
                   )}
               </div>
